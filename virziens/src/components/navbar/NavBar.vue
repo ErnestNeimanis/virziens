ccf97b46b4b8b1ae55e79b5f0ccfb80e64029484
[neimanis] <[ernestsneimanis]>
2023-04-03 19:00:30 +0200

    innn

--- virziens/src/components/navbar/NavBar.vue
+++ virziens/src/components/navbar/NavBar.vue
@@ -3,5 +3,12 @@
 </script>
 
 <template>
-    <div class="w-full h-36 bg-blue-600"></div>
+    <div class="w-full h-36 ">
+        <div class="relative w-full h-96 border-4 border-black overflow-hidden">
+            <video autoplay muted loop class="absolute top-0 left-0 objec-cover object-center z-0">
+                <source src="@/assets/videos/janepatikneskaties.mp4" type="video/mp4">
+            </video>
+        </div>
+
+    </div>
 </template>
