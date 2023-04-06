128d762ac43ecf87821af29f48110d853c5c4590
[neimanis] <[ernestsneimanis]>
2023-04-06 07:06:14 +0200

    build fix

--- virziens/src/views/HomeView.vue
+++ virziens/src/views/HomeView.vue
@@ -122,7 +122,7 @@ onBeforeUnmount(() => {
     </div>
 
        <div v-if="secondVidSectionVisible"   class="relative h-[60vw] w-full z-[-100]">
-        <div class="w-full fixed bottom-0 ">
+        <div class="w-full fixed sm:bottom-0 ">
           <video class="w-full" autoplay muted loop>
             <source src="@/assets/videos/one-dancer.mp4" type="video/mp4"/>
           </video>
@@ -136,8 +136,19 @@ onBeforeUnmount(() => {
 
     <HomeSection1 />
     <HomeSection2 />
-    <section class="w-full h-[60vw]">
-
+    <section class="w-full min-h-[60vw]">
+      <div class=" h-full flex flex-col justify-center items-center md:gap-20">
+       
+       <p class="bg-white/30 px-9 xl:text-9xl md:text-7xl xs:text-4xl text-3xl text-black font-extrabold font-serif  ">Baiba Andersone jums iemācīs dejot kā neviens cits!</p>
+       
+       <div class="flex justify-end w-full px-20">
+  
+<button class="bg-black rounded-full hover:bg-black/30 mt-6  px-4 py-2 text-white uppercase font-extrabold">random poga</button>
+      
+ 
+       </div>
+      
+      </div>
     </section>
     <div ref="secondVidSection">
  <HomeSection3 />
@@ -145,6 +156,8 @@ onBeforeUnmount(() => {
    
 
       <section   class=" w-full h-[60vw]">
+
+        
 
     </section>
 
