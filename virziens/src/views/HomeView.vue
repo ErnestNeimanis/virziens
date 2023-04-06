8f0fdebe2d9001addff18835721d79a3b7413c32
[neimanis] <[ernestsneimanis]>
2023-04-06 23:38:01 +0200

    testing scroll problem

--- virziens/src/views/HomeView.vue
+++ virziens/src/views/HomeView.vue
@@ -12,105 +12,17 @@ import HomeSection4 from "@/components/home/HomeSection4.vue"
 import BGE1 from "@/components/home/BGE1.vue"
 import HTSection1 from "@/components/home/HTSection1.vue"
 
-const translate = ref(0)
-
-const translate2 = ref(0)
 
 
-const secondVidSection = ref<HTMLAreaElement | null>(null);
-
-const secondVidSectionVisible = ref(false)
-
-
-
-function handleScroll() {
-  if (!secondVidSection.value) return
-  const elementPosition = secondVidSection.value.getBoundingClientRect();
-
-  if (
-
-    elementPosition.bottom < window.innerHeight
-
-  ) {
-    secondVidSectionVisible.value = true
-  } else {
-    secondVidSectionVisible.value = false
-  }
-}
-
-onMounted(() => {
-  console.log(secondVidSection.value)
-  window.addEventListener('scroll', handleScroll);
-});
-
-// var screenHeight = window.innerHeight;
-
-// window.addEventListener('resize', function() {
-//   screenHeight = window.innerHeight;
-//   console.log("Screen height changed to: " + screenHeight);
-// });
-
-
-// var prevScrollpos = window.pageYOffset;
-
-// window.onscroll = function() {
-//   const currentScrollPos = window.pageYOffset;
-//   console.log(currentScrollPos)
-//   if (prevScrollpos > currentScrollPos) {
-
-//     //console.log("Scrolling up");
-//   } else {
-
-//     //console.log("Scrolling down");
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
-function parallax() {
-  const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
-  const scrollPercent = Math.floor((window.scrollY / scrollableHeight) * 100);
-
-
-  //console.log(scrollPercent)
-
-
-
-
-  if (window.innerWidth < 800) {
-    translate.value = scrollPercent * 0.5
-  } else if (window.innerWidth < 600) {
-    translate.value = scrollPercent * 0.1
-  } else {
-    translate.value = scrollPercent * 0.8
-  }
-
-
-}
-
-
-onMounted(() => {
-
-  window.addEventListener('scroll', parallax);
-})
-onBeforeUnmount(() => {
-
-})
-
-//:style="{ transform: `translateY(${-translate}%)` }"
 </script>
 <template>
-  <main class="">
+ 
     <NavBar class=" fixed w-full top-8 right-4 z-[100]" />
-    <!-- <div v-if="secondVidSectionVisible"   class="relative h-[60vw] w-full z-[-100]">
-              <div class="w-full fixed sm:bottom-0 bottom-12 ">
-                <video class="w-full" autoplay muted loop>
-                  <source src="@/assets/videos/one-dancer.mp4" type="video/mp4"/>
-                </video>
-              </div>
-            </div> -->
 
 
-    <div class="wrapper  h-[100vh] overflow-y-auto  ">
+
+
+    <div class="wrapper  h-[100vh]  ">
      
       <div class="h-full w-full background ">
 <HomeBGVideo class=" " />
@@ -118,6 +30,13 @@ onBeforeUnmount(() => {
 
         </div>
         <BGE1 class="" />
+
+          <div class="w-full ">
+                <video class="w-full" autoplay muted loop>
+                  <source src="@/assets/videos/one-dancer.mp4" type="video/mp4"/>
+                </video>
+              </div>
+
       </div>
       
       <div class="foreground ">
@@ -137,25 +56,33 @@ onBeforeUnmount(() => {
 
 
 
-  </main>
+
 </template>
 
 <style>
 .wrapper {
-  height: 100vh;
-  overflow-y: auto;
+  background-color: black;
+  height: 100%;
+  overflow-y: scroll;
+  scroll-behavior: smooth;
   perspective: 10px;
+ 
+   
 }
 
 .background {
   transform: translateZ(-10px) scale(2)
 }
+.foreground{
 
+}
 
 .background,
 .foreground {
   position: absolute;
   height:100%;
+  width: 100%;
+  object-fit: cover;
  
   z-index: -1;
 }
