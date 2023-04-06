3ccdda2d6afbf504487176b5ba40e18a36208f6f
[neimanis] <[ernestsneimanis]>
2023-04-06 06:43:31 +0200

    second vid

--- virziens/src/components/home/HomeSection2.vue
+++ virziens/src/components/home/HomeSection2.vue
@@ -5,20 +5,20 @@
   <section class=" w-full   ">
 
     <div class="flex justify-center description-text  ">
-      <h1 class=" xl:text-9xl md:text-7xl xs:text-4xl text-3xl px-6 uppercase font-extrabold ">kautkāds īss apraksts par
-        mums </h1>
+      <h1 class=" xl:text-9xl md:text-7xl xs:text-4xl text-3xl px-6 uppercase font-extrabold ">vieta kur dejas kļūst par dzīvesveidu </h1>
     </div>
     <div class="flex justify-start items-center h-full relative overflow-hidden ] ">
-      <div class="relative flex justify-center pt-16  bg-white/30 w-full  lg:min-h-[1200px] md:min-h-[900px] sm:min-h-[600px] min-h-[450px]  min-w-[300px] ">
-        <article class=" flex flex-col md:gap-16 gap-4  lg:text-3xl sm:text-2xl text-xl px-6 font-bold   h-full ">
-          <p>
+      <div class="relative flex justify-center items-center  bg-white/30 w-full  lg:min-h-[1200px] md:min-h-[900px] sm:min-h-[600px] min-h-[450px]  min-w-[300px] ">
+        <article class=" flex flex-col md:gap-16 gap-4  lg:text-3xl sm:text-xl text-base  px-6   h-full ">
+          <p class="font-semibold font-serif ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad architecto expedita, aperiam molestias ab
-            aspernatur
-           -
+            aspernatur Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, tempora!
+           
           </p>
         
-          <p class="text-right">
+          <p class="w-full text-right text-white rounded-lg px-6 font-extrabold bg-white/15 shadow-2xl shadow-white/70">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, molestias.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptas sit fuga nostrum hic esse, facilis magnam mollitia. Modi, atque.
 
           </p>
 
--- virziens/src/views/HomeView.vue
+++ virziens/src/views/HomeView.vue
@@ -12,9 +12,34 @@ import HomeSection4 from "@/components/home/HomeSection4.vue"
 
 const translate = ref(0)
 
+const translate2 = ref(0)
+
+
+const secondVidSection = ref<HTMLAreaElement | null>(null);
+
+const secondVidSectionVisible = ref(false)
 
 
 
+    function handleScroll() {
+      if(!secondVidSection.value) return
+      const elementPosition = secondVidSection.value.getBoundingClientRect();
+
+      if (
+ 
+        elementPosition.bottom < window.innerHeight 
+       
+      ) {
+        secondVidSectionVisible.value = true
+      }else{
+         secondVidSectionVisible.value = false
+      }
+    }
+
+    onMounted(() => {
+      console.log(secondVidSection.value)
+      window.addEventListener('scroll', handleScroll);
+    });
 
 // var screenHeight = window.innerHeight;
 
@@ -43,9 +68,19 @@ function parallax() {
   const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
   const scrollPercent = Math.floor((window.scrollY / scrollableHeight) * 100);
 
-  
 
-  translate.value = scrollPercent * 1.5
+console.log(scrollPercent)
+
+if(window.innerWidth < 800){
+ translate.value = scrollPercent * 0.5
+}else if(window.innerWidth < 600){
+   translate.value = scrollPercent * 0.1
+} else{
+   translate.value = scrollPercent * 0.8
+}
+ 
+
+ 
 
 }
 
@@ -63,37 +98,58 @@ onBeforeUnmount(() => {
 <template>
   <main class="">
 
-    <div :style="{ transform: `translateY(${-translate}vw)` }" class="parallax-duration  w-full fixed top-0 left-0 z-[-1]">
-      <HomeBGVideo class=" " />
+    <div :style="{ transform: `translateY(${-translate}%)` }"  class="parallax-duration  w-full fixed top-0 left-0 z-[-100]">
+      
+      <HomeBGVideo class="z-[-100] " />
+
       <div class=" ">
- <div class="pt-[250px] bg-rose-100 w-[160vw] pr-[20vw]">
-    <div class="w-full h-[100px] bg-black">
+        <div class="pt-[400px] bg-rose-100 w-[170vw] pr-[20vw]">
+          <div class="w-full h-[50px] bg-black">
+
+          </div>
+          <img src="@/assets/images/ba-stars.jpg" alt="" class="w-full">
+       
+        </div>
+        <div class="relative h-[50vh] bg-black z-[-3000]">
+
+        </div>
+      </div>
+
+   
+
+
 
     </div>
-        <img src="@/assets/images/ba-stars.jpg" alt="" class="w-full">
-      </div>
-      </div>
-     
 
-
-    </div>
+       <div v-if="secondVidSectionVisible"   class="relative h-[60vw] w-full z-[-100]">
+        <div class="w-full fixed bottom-0 ">
+          <video class="w-full" autoplay muted loop>
+            <source src="@/assets/videos/one-dancer.mp4" type="video/mp4"/>
+          </video>
+        </div>
+      </div>
 
 
     <NavBar class=" fixed w-full top-8 right-4 z-[100]" />
 
 
 
-    <HomeSection1  />
+    <HomeSection1 />
     <HomeSection2 />
     <section class="w-full h-[60vw]">
 
     </section>
-    <HomeSection3 />
+    <div ref="secondVidSection">
+ <HomeSection3 />
+    </div>
+   
+
+      <section   class=" w-full h-[60vw]">
+
+    </section>
+
     <HomeSection4 />
-
-
-
-
+  
 
 
   </main>
@@ -109,7 +165,7 @@ onBeforeUnmount(() => {
 
 
 .parallax-duration {
-  transition-duration: 100ms;
+  transition-duration: 150ms;
 }
 
 
