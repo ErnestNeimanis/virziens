1c747373572f1311e5b8abfab25b46d79c87a6dd
[neimanis] <[ernestsneimanis]>
2023-04-07 17:01:07 +0200

    contact info

---
+++ virziens/src/components/ContactCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+<template>
+<div class=" rounded-lg px-4 py-3 ">
+  <p class="text-white font-medium mb-2">Contact Information</p>
+  <ul class="text-white text-sm">
+    <li class="flex items-center mb-1">
+     <i class="bi bi-geo-alt-fill"></i>
+      <span class="ml-2" >Jūrmala,Latvija</span>
+    </li>
+    <li class="flex items-center mb-1">
+      <i class="bi bi-telephone"></i>
+      <a class="ml-2" href="tel:+1234567890">1+371 29 445 989</a>
+    </li>
+    <li class="flex items-center">
+     <i class="bi bi-envelope-open"></i>
+      <a  class="ml-2" href="mailto:example@example.com">dejustudijavirziens@gmail.com</a>
+    </li>
+  </ul>
+</div>
+
+</template>
--- virziens/src/components/home/HomeSection4.vue
+++ virziens/src/components/home/HomeSection4.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import ContactCard from '../ContactCard.vue';
 </script>
 <template>
-      <section class=" w-full h-96 pt-4 bg-black">
-      <div class="w-full">
+      <section class="flex flex-col items-center justify-start w-full h-96 pt-4 bg-black min-w-[300px] ">
+     
+      
+      <div class="w-full -translate-y-32">
         <div class="flex justify-center items-center gap-4">
           <div> <a href="https://www.facebook.com/dejustudijavirziens?locale=de_DE"> <i
                 class="bi bi-facebook text-white hover:text-gray-400 text-3xl"></i> </a></div>
@@ -11,6 +13,7 @@
                 class="bi bi-instagram text-white text-3xl hover:text-gray-400"></i> </a></div>
         </div>
       </div>
+        <ContactCard/>
 
 
     </section>
