b6232478360264cb6346159d4bf3cab6668b2318
[neimanis] <[ernestsneimanis]>
2023-04-07 16:44:56 +0200

    transitions working

--- virziens/src/components/home/HomeSection2.vue
+++ virziens/src/components/home/HomeSection2.vue
@@ -1,31 +1,98 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 
+const firstText = ref<HTMLElement | null>(null)
+const firstTextAnimationClass = ref("first-text")
+
+const secondText = ref<HTMLElement | null>(null)
+const secondTextAnimationClass = ref<string>("translate-x-[10vw]")
+
+function animateFirstText() {
+  firstTextAnimationClass.value = "first-text-animate"
+}
+
+function animateSecondText1() {
+  secondTextAnimationClass.value = ""
+}
+function animateSecondText2() {
+  secondTextAnimationClass.value = "shadow-2xl shadow-white"
+}
+
+
+function onElementIntersection(element: HTMLElement, 
+myCallbackFunction: (entry: IntersectionObserverEntry) 
+=> void, threshold: number | undefined = 0.1) {
+  if (!threshold) threshold = 0.1
+
+  const options = {
+    rootMargin: '0px',
+    threshold: threshold
+  };
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio >= options.threshold) {
+        myCallbackFunction(entry);
+      }
+    });
+  }, options);
+
+  observer.observe(element);
+}
+
+
+
+function inViewport(el: HTMLElement | null): boolean {
+  if (!el) throw new Error("no element detected")
+  const rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+onMounted(() => {
+  if (!firstText.value || !secondText.value) throw new Error("mounted but no element")
+  onElementIntersection(firstText.value, animateFirstText)
+  onElementIntersection(secondText.value, animateSecondText1)
+  onElementIntersection(secondText.value, animateSecondText2, 1)
+})
 </script>
 <template>
   <section class=" w-full   ">
 
     <div class="flex justify-center description-text  ">
-      <h1 class=" xl:text-9xl md:text-7xl xs:text-4xl text-3xl px-6 uppercase font-extrabold text-center ">vieta kur dejas kļūst par dzīvesveidu </h1>
+      <h1 class=" xl:text-9xl md:text-7xl xs:text-4xl text-3xl px-6 uppercase font-extrabold text-center  ">vieta kur
+        dejas kļūst par dzīvesveidu </h1>
     </div>
     <div class="flex justify-start items-center h-full relative overflow-hidden ] ">
-      <div class="relative flex  bg-white/30 w-full  lg:min-h-[1200px] md:min-h-[900px] sm:min-h-[600px] min-h-[450px]  min-w-[340px] ">
-        <article class="flex flex-col mt-[25vw] md:gap-[10vw] gap-4  lg:text-5xl md:text-4xl sm:text-2xl xs:text-base  px-6   h-full ">
-          <p class="font-semibold font-serif ">
+      <div
+        class="relative flex  bg-white/30 w-full  lg:min-h-[1200px] md:min-h-[900px] sm:min-h-[600px] min-h-[450px]  min-w-[340px] ">
+        <article
+          class="flex flex-col mt-[25vw] md:gap-[10vw] gap-4  lg:text-5xl md:text-4xl sm:text-2xl xs:text-base  px-6   h-full ">
+          <p :class="firstTextAnimationClass" ref="firstText" class=" font-semibold font-serif ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad architecto expedita, aperiam molestias ab
             aspernatur Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, tempora!
-           
+
           </p>
-        
-          <p class="w-full text-right text-white rounded-lg px-6 font-extrabold bg-white/15 shadow-2xl shadow-white/70">
+
+          <p :class="secondTextAnimationClass" ref="secondText"
+            class="  transition-all  w-full text-right text-white rounded-lg px-6 font-extrabold bg-white/15 "
+            style="transition-duration: 800ms;">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, molestias.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptas sit fuga nostrum hic esse, facilis magnam mollitia. Modi, atque.
-            Lorem 
-          
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptas sit fuga nostrum hic esse, facilis magnam
+            mollitia. Modi, atque.
+            Lorem
+
           </p>
 
 
         </article>
-        <img class="background-pattern  absolute top-0 w-full z-[-1] -translate-y-[40vw]" src="@/assets/images/two-brown-dancers.jpg" alt="">
+        <img class="background-pattern  absolute top-0 w-full z-[-1] -translate-y-[40vw]"
+          src="@/assets/images/two-brown-dancers.jpg" alt="">
       </div>
 
 
@@ -41,10 +108,34 @@
   animation-fill-mode: forwards;
 }
 
+.first-text {
+  transform: translateX(-20px);
+}
+
+.first-text-animate {
+
+  animation-name: slide-in;
+  animation-duration: 1s;
+
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(-10rem);
+  }
+
+  to {
+    transform: translateX(0);
+  }
+
+}
 
 .background-pattern {
   background-image: url('@/assets/images/pattern-large.png');
 }
+
+
+
 
 @keyframes fade-out {
   from {
@@ -55,5 +146,4 @@
   to {
     background-color: transparent;
   }
-}
-</style>
+}</style>
--- virziens/src/components/home/HomeSection3.vue
+++ virziens/src/components/home/HomeSection3.vue
@@ -1,9 +1,95 @@
 <script setup lang="ts">
+import { onBeforeMount, onBeforeUnmount, onMounted, ref } from 'vue';
+
+const parentSection = ref(null)
+
+
+const img1opacity = ref("opacity-1")
+
+const imgOpacityArray =  ref<string[]>([])
+const imgPathArray = ref<string[]>([
+    "@/assets/images/brown-dancers-halfpyramid.jpg",
+    "@/assets/images/virziens-piramida.jpg",
+    "@/assets/images/balerina.jpg"
+])
+const indexGenerator = imgIndexes(imgPathArray.value.length)
+function setOpacity(){
+   if(img1opacity.value === "opacity-1") img1opacity.value = "opacity-0"
+   else img1opacity.value = "opacity-1"
+
+}
+
+const intervalId = ref<any>(null)
+const intervalStarted =  ref(false)
+
+
+function startTimeout() { 
+  
+   if(!intervalStarted.value){
+     intervalId.value= setInterval(setOpacity,3000)
+     intervalStarted.value = true;
+   }
+  
+}
+function stopTimeout() {
+    if(!intervalId.value) return
+window.clearInterval(intervalId.value)
+}
+
+onMounted(() => {
+    if(!parentSection.value) return
+   
+    onElementIntersection(parentSection.value,startTimeout,0.3)
+    
+
+  
+})
+onBeforeUnmount(()=>{
+    stopTimeout()
+})
+
+function* imgIndexes(length:number = 0){
+    let n = 0;
+    while(n < length){
+        yield n;
+        if(n+1 === length) n = 0;
+        else n++;
+    }
+}
+
+function onElementIntersection(element: HTMLElement, 
+myCallbackFunction: (entry: IntersectionObserverEntry) 
+=> void, threshold: number | undefined = 0.1) {
+  if (!threshold) threshold = 0.1
+
+  const options = {
+    rootMargin: '0px',
+    threshold: threshold
+  };
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio >= options.threshold) {
+        myCallbackFunction(entry);
+      }
+    });
+  }, options);
+
+  observer.observe(element);
+}
+
+
 
 </script>
 <template>
-    <section class="relative w-full h-[60vw] ">
-        <img class=" absolute top-0 min-w-full z-[]" src="@/assets/images/virziens-piramida.jpg" alt="">
+    <section ref="parentSection" class="relative w-full h-[60vw] ">
+        
+          <img  class="img-switch-transition  absolute top-0 min-w-full " src="@/assets/images/brown-dancers-halfpyramid.jpg" alt="">
+           <img  :class="img1opacity" class="img-switch-transition  absolute top-0 min-w-full " src="@/assets/images/virziens-piramida.jpg" alt="">
+       
+      
+        
+
 
 
         <div class="relative w-full h-full flex justify-center items-center z-[1] ">
@@ -16,3 +102,9 @@
 
     </section>
 </template>
+<style>
+.img-switch-transition{
+    transition: opacity;
+    transition-duration: 2.5s;
+}
+</style>
