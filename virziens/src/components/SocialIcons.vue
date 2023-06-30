7847d67bad2086230b1dcb114635110ec0cf5012
[neimanis] <[ernestsneimanis]>
2023-06-30 03:59:13 +0200

    smaller soc icons

--- virziens/src/components/SocialIcons.vue
+++ virziens/src/components/SocialIcons.vue
@@ -11,10 +11,10 @@ const show = computed(() => props.show ?? false)
 <template>
  <div class="flex justify-center gap-8 transition-all duration-500  rounded-full py-2 " :class="{'opacity-0 translate-y-36':!show}">
                 <div> <a href="https://www.facebook.com/dejustudijavirziens?locale=de_DE"> <i
-                      class="bi bi-facebook text-white hover:text-gray-400 text-8xl"></i> </a>
+                      class="bi bi-facebook text-white hover:text-gray-400 text-5xl"></i> </a>
                 </div>
                 <div> <a href="https://www.instagram.com/dejustudijavirziens/?hl=de"> <i
-                      class="bi bi-instagram text-white text-8xl hover:text-gray-400"></i> </a>
+                      class="bi bi-instagram text-white text-5xl hover:text-gray-400"></i> </a>
                 </div>
               </div>
 </template>
