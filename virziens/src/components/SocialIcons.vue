7367302a7272f160e72b199814ccfdcb0a5c2dd3
[neimanis] <[ernestsneimanis]>
2023-06-30 07:07:45 +0200

    social buttons pointer events fixed

--- virziens/src/components/SocialIcons.vue
+++ virziens/src/components/SocialIcons.vue
@@ -9,11 +9,11 @@ const props = defineProps<
 const show = computed(() => props.show ?? false)
 </script>
 <template>
- <div class="flex justify-center gap-8 transition-all duration-500  rounded-full py-2 " :class="{'opacity-0 translate-y-36':!show}">
-                <div> <a href="https://www.facebook.com/dejustudijavirziens?locale=de_DE"> <i
+ <div class="flex justify-center gap-8 transition-all duration-500  rounded-full py-2 pointer-events-none" :class="{'opacity-0 translate-y-36':!show}">
+                <div class="pointer-events-auto"> <a href="https://www.facebook.com/dejustudijavirziens?locale=de_DE"> <i
                       class="bi bi-facebook text-white hover:text-gray-400 text-5xl"></i> </a>
                 </div>
-                <div> <a href="https://www.instagram.com/dejustudijavirziens/?hl=de"> <i
+                <div class="pointer-events-auto"> <a href="https://www.instagram.com/dejustudijavirziens/?hl=de"> <i
                       class="bi bi-instagram text-white text-5xl hover:text-gray-400"></i> </a>
                 </div>
               </div>
