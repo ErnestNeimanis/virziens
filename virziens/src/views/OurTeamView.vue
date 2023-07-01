e7c34b624adaf6d2ff2d065cc67af19bbe3646e5
[neimanis] <[ernestsneimanis]>
2023-07-01 17:24:03 +0200

    added toml

---
+++ virziens/src/views/OurTeamView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+interface TeamMember {
+    name: string;
+    subtitle?: string;
+    image?: string;
+    text?: string;
+
+}
+
+const teamMembers = 
+[
+    {
+        name: "",
+        subtitle: "",
+        image: "@/assets/images/nosaukums.jpg",
+        text: `
+        
+        teksta html
+
+        `,
+    },
+]
+
+
+
+</script>
+<template>
+
+</template>
---
+++ virziens/src/views/ServicesView.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+
+interface Service {
+    title: string;
+    text?: string;
+    prices?: {
+        amount: string | number,
+        description?: string
+    }[];
+
+}
+
+const services = 
+[
+    {
+        title: "",
+        prices: 
+        [
+            {
+                amount: "",
+                description: "",
+            },
+             {
+                amount: "",
+                description: "",
+            },
+             {
+                amount: "",
+                description: "",
+            },
+        ],
+        text: "",
+    },
+    {
+        title: "",
+        prices: 
+        [
+            {
+                amount: "",
+                description: "",
+            },
+        ],
+        text: "",
+    },
+]
+
+
+</script>
+<template>
+
+</template>
