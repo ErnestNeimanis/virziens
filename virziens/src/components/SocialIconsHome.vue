98bd89da0dcd2fa37499f0943ea9465dd450c8ab
[neimanis] <[ernestsneimanis]>
2024-03-15 17:43:29 +0100

    button design

--- virziens/src/components/SocialIconsHome.vue
+++ virziens/src/components/SocialIconsHome.vue
@@ -1,38 +1,28 @@
 <script setup lang="ts">
+import { computed, onMounted, onUnmounted, ref } from "vue";
 import { useWindowSize } from "../composables/window";
 import { useDeviceStore } from "../stores/device.store";
-import { computed, onMounted, onUnmounted, ref } from 'vue';
-
+const props = defineProps<{
+      show?: boolean;
+}>();
 
 const {smallWindow,mediumWindow,largeWindow} = useWindowSize();
 const device = useDeviceStore();
 device.setDeviceType();
 
-const props = defineProps<
-    {
-        show?: boolean
-    }
->()
-
-const show = ref<boolean>(false)
+const show = ref<boolean>(false);
 const windowY = ref(window.scrollY);
 
-
-
 function handleScroll() {
+      const currentScrollPos = window.scrollY;
+    
+      if (currentScrollPos > windowY.value) {
+            show.value = true;
+      } else {
+            show.value = false;
+      }
 
-    const currentScrollPos = window.scrollY;
-    console.log(currentScrollPos, windowY.value)
-    if (currentScrollPos > windowY.value) {
-
-        show.value = true
-    } else {
-
-        show.value = false;
-    }
-
-
-    windowY.value = currentScrollPos;
+      windowY.value = currentScrollPos;
 }
 
 function telHref(phone: string | undefined) {
@@ -41,33 +31,28 @@ function telHref(phone: string | undefined) {
 }
 
 onMounted(() => {
-    if (!props.show) {
-        window.addEventListener("scroll", handleScroll)
-    }
+      if (!props.show) {
+            window.addEventListener("scroll", handleScroll);
+      }
+});
+onUnmounted(() => window.removeEventListener("scroll", handleScroll));
 
-})
-onUnmounted(() => window.removeEventListener("scroll", handleScroll))
-
+const socialLinks = ref([
+  { icon: 'bi-facebook', link: 'https://www.facebook.com/dejustudijavirziens' },
+  { icon: 'bi-instagram', link: 'https://www.instagram.com/dejustudijavirziens' },
+  { icon: 'bi-telephone-outbound', link: telHref('+37129445989') },
+]);
 </script>
 <template>
-    <div class="fixed bottom-0 z-[100] w-full flex justify-center gap-8 transition-all duration-1000 bg-black py-2 pointer-events-none"
+     <div class="fixed top-24 right-0 z-[100]  flex flex-col items-end justify-center gap-3 transition-all duration-1000 b py-2 pointer-events-none"
             :class="{ 'translate-y-24': !props.show }">
-            <div class="flex justify-between px-14" :class="{'w-full px-3':smallWindow || !device.isDesktop,'w-1/6  min-w-[300px]':!smallWindow}">
+            <div v-for="icon in socialLinks" :key="icon" class="flex justify-between px-14 bg-red-950 rounded-sm p-1 mr-1" :class="{'w-full px-3':smallWindow || !device.isDesktop,'w-1/6  min-w-[300px]':!smallWindow}">
                   <div class="pointer-events-auto">
-                        <a href="https://www.facebook.com/dejustudijavirziens">
-                              <i class="bi bi-facebook text-white hover:text-gray-400 text-5xl"></i>
+                        <a :href="icon.link">
+                              <i :class="icon.icon" class=" text-white hover:text-gray-400 text-4xl"></i>
                         </a>
                   </div>
-                  <div class="pointer-events-auto">
-                        <a href="https://www.instagram.com/dejustudijavirziens">
-                              <i class="bi bi-instagram text-white text-5xl hover:text-gray-400"></i>
-                        </a>
-                  </div>
-                  <div class="pointer-events-auto">
-                        <a :href="telHref('+37129445989')">
-                              <i class="bi bi-telephone-outbound text-white text-5xl hover:text-gray-400"></i>
-                        </a>
-                  </div>
+            
             </div>
 
       </div>
