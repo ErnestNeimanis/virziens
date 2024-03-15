04666c037f75b9bfba05d36a2628e519f80b7573
[neimanis] <[ernestsneimanis]>
2024-03-15 15:37:44 +0100

    test push

--- virziens/src/components/SocialIcons.vue
+++ virziens/src/components/SocialIcons.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, onMounted, onUnmounted, ref } from "vue";
-import { useWindowSize } from "@/composables/window";
-import { useDeviceStore } from "@/stores/device.store";
+import { useWindowSize } from "../composables/window";
+import { useDeviceStore } from "../stores/device.store";
 const props = defineProps<{
       show?: boolean;
 }>();
