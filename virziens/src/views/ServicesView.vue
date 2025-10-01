9a69c3af6d46b780619c611753372a1f78452c38
pixelmirai <pixelmirai@gmail.com>
2025-10-01 23:18:23 +0200

    PIEAUGUSIE added

--- virziens/src/views/ServicesView.vue
+++ virziens/src/views/ServicesView.vue
@@ -267,8 +267,21 @@ const section3 = [
               <div class="mt-5 ">
                 <h2 class="text-lg font-extrabold">Maksa par deju nodarbībām grupā – LADY STYLE:</h2>
                 <ul class="list-disc pl-5 mt-2 ">
+
                   <li>70.00 EUR abonements uz 8 nodarbībām (bez laika ierobežojuma)</li>
                   <li>12.00 EUR 1 nodarbības apmeklējums</li>
+                  <li>BEZ MAKSAS - pirmā izmēģinājuma nodarbība</li>
+
+                </ul>
+
+              </div>
+
+              <div class="mt-5 ">
+                <h2 class="text-lg font-extrabold">PIEAUGUŠIE</h2>
+                <ul class="list-disc pl-5 mt-2 ">
+
+                  <li>60.00 EUR abonements uz 8 nodarbībām (bez laika ierobežojuma)</li>
+                  <li>10.00 EUR 1 nodarbības apmeklējums</li>
                   <li>BEZ MAKSAS - pirmā izmēģinājuma nodarbība</li>
 
                 </ul>
