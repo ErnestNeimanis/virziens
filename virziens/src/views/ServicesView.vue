a2dd96647cc202b459814379d4cd508fc4a65689
Evijass <163547734+Evijass@users.noreply.github.com>
2025-10-02 18:35:56 +0300

    Update ServicesView.vue

--- virziens/src/views/ServicesView.vue
+++ virziens/src/views/ServicesView.vue
@@ -277,10 +277,10 @@ const section3 = [
               </div>
 
               <div class="mt-5 ">
-                <h2 class="text-lg font-extrabold">PIEAUGUŠIE</h2>
+                <h2 class="text-lg font-extrabold">Maksa par deju nodarbībām grupā – PIEAUGUŠIE</h2>
                 <ul class="list-disc pl-5 mt-2 ">
 
-                  <li>60.00 EUR abonements uz 8 nodarbībām (bez laika ierobežojuma)</li>
+                  <li>65.00 EUR abonements uz 8 nodarbībām (bez laika ierobežojuma)</li>
                   <li>10.00 EUR 1 nodarbības apmeklējums</li>
                   <li>BEZ MAKSAS - pirmā izmēģinājuma nodarbība</li>
 
