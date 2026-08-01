e03c6b86c39530be391c9a6d4ce833f4aacd87ff
Evijass <163547734+Evijass@users.noreply.github.com>
2026-08-01 14:32:43 +0300

    Update ScheduleHeading.vue
    Informācijas atjaunošana

--- virziens/src/components/Schedule/ScheduleHeading.vue
+++ virziens/src/components/Schedule/ScheduleHeading.vue
@@ -3,6 +3,6 @@
 </script>
 <template>
     <div class="w-full">
-        <h1 class=" text-center font-extrabold text-[5.5vw] text-yellow-300 uppercase">Nodarbību grafiks 2025./2026. </h1>
+        <h1 class=" text-center font-extrabold text-[5.5vw] text-yellow-300 uppercase">Nodarbību grafiks 2026./2027. </h1>
     </div>
 </template>
